ee258f91695600e33910200c32c4d722594443a3
ChrisLenovo <chrisk357@gmail.com>
2019-08-15 21:30:22 -0500

    secondattempt

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import static java.lang.Integer.sum;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a = 5;
+        int b = 3;
+
+        System.out.println(sum(a,b));
+
+    }
+    static int retSum(int x, int y)
+    {
+        int sum = x + y;
+        int num1 = x;
+        int num2 = y;
+
+        return sum;
+    }
+}
